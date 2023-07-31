07ae75447823f4bdced2c943dc52dc916047d598
MarekZiemba <ziembamarek@gmail.com>
2023-07-31 18:24:56 +0200

    MOD: added password validation

--- portfolioLabCharity/src/main/java/pl/coderslab/charity/security/UserRegistrationDto.java
+++ portfolioLabCharity/src/main/java/pl/coderslab/charity/security/UserRegistrationDto.java
@@ -2,6 +2,7 @@ package pl.coderslab.charity.security;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -25,11 +26,9 @@ public class UserRegistrationDto {
     @Size(min = 2, message = "{too.short.error}")
     private String username;
 
-//    @NotBlank(message = "{not.empty.error}")
-//    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$",
-//            message = "{password.invalid.error}")
-//    private  String password;
-
+    @NotBlank(message = "{not.empty.error}")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$",
+            message = "{password.invalid.error}")
     private String newPassword;
 
     private String confirmPassword;
